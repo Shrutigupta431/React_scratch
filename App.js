9bc67aa06d0dbfab6432a2fb0947cf9b2d99783c
shruuGupta <sg9956855@gmail.com>
2024-02-12 11:40:37 +0530

    episode2

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+// const heading = React.createElement("h1", {id:"header" , xyz:"abc"}, "Hello world");
+
+// const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am a h1 tag")))
+// const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am a h1 tag"),React.createElement("h2",{},"I am a h2 tag")]))
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "Namaste React"),
+      React.createElement("h2", {}, "I am a h2 tag"),
+    ]),
+  ]),
+]);
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
